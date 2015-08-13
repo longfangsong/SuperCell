d3e64d931e5d7c8130769588e54d2cc5d73596c9
longfangsong <longfangsong@icloud.com>
2015-08-13 16:01:03 +0800

    Initial commit

---
+++ cell.py
@@ -0,0 +1,316 @@
+import random
+
+
+class Cell:
+    """
+    细胞的Model部分
+    """
+    max_experience = 65536
+
+    def __init__(self, father=None, mother=None):
+        self.__hp_max = 5
+        self.__hp = 5
+        self.__hp_variation_rate = 0.1
+        self.__energy_max = 10
+        self.__energy = 10
+        self.__energy_variation_rate = 0.1
+        self.__attack = 5
+        self.__attack_variation_rate = 0.1
+        self.__defence = 3
+        self.__defence_variation_rate = 0.1
+        self.__variation_rate = 0.01
+        self.__breed_will = 1 / 20
+        self.__experience = 0
+        self.__level = 1
+        self.__bad = False
+        self.__harm_of_round = 0
+        if father and mother:
+            # 各项个体值随机来自某个亲本
+            assert father.__bad == mother.__bad
+            assert father.__energy_max == father.__energy and mother.__energy_max == mother.__energy
+            self.__hp_variation_rate = (father if random.randint(0, 1) == 0 else mother).__hp_variation_rate
+            self.__energy_variation_rate = (father if random.randint(0, 1) == 0 else mother).__energy_variation_rate
+            self.__attack_variation_rate = (father if random.randint(0, 1) == 0 else mother).__attack_variation_rate
+            self.__defence_variation_rate = (father if random.randint(0, 1) == 0 else mother).__defence_variation_rate
+            self.__breed_will = (father if random.randint(0, 1) == 0 else mother).__breed_will
+            self.__variation_rate = (father if random.randint(0, 1) == 0 else mother).__variation_rate
+            # 提升双亲能力值之和最高的那项的个体值
+            elements = [
+                father.__hp_max + mother.__hp_max,
+                father.__energy_max + mother.__energy_max,
+                father.__defence + mother.__defence,
+                father.__attack + mother.__attack,
+                father.__breed_will * 2 + mother.__breed_will * 2,
+                father.__variation_rate * 10 + mother.__variation_rate * 10
+            ]
+            if max(elements) == elements[0]:  # hp
+                self.__hp_variation_rate *= 1.1
+            elif max(elements) == elements[1]:  # energy
+                self.__energy_variation_rate *= 1.1
+            elif max(elements) == elements[2]:  # defence
+                self.__defence_variation_rate *= 1.1
+            elif max(elements) == elements[3]:  # attack
+                self.__attack_variation_rate *= 1.1
+            elif max(elements) == elements[4]:  # breed_will
+                self.__breed_will = (self.__breed_will * 2 * 1.1) / 2
+            elif max(elements) == elements[5]:  # variation
+                self.__variation_rate = (self.__variation_rate * 10 * 1.1) / 10
+                if self.__variation_rate > 0.35:
+                    self.__variation_rate = 0.35
+            # 随机提升一项个体值
+            element = random.randint(0, 5)
+            if element == 0:
+                self.__hp_variation_rate *= 1.05
+            elif element == 1:
+                self.__hp_variation_rate *= 1.05
+            elif element == 2:
+                self.__defence_variation_rate *= 1.05
+            elif element == 3:
+                self.__attack_variation_rate *= 1.05
+            elif element == 4:
+                self.__breed_will = (self.__breed_will * 2 * 1.05) / 2
+            elif element == 5:
+                self.__variation_rate = (self.__variation_rate * 10 * 1.05) / 10
+            # 随机降低一项个体值
+            element = random.randint(0, 5)
+            if element == 0:
+                self.__hp_variation_rate *= 0.95
+            elif element == 1:
+                self.__hp_variation_rate *= 0.95
+            elif element == 2:
+                self.__defence_variation_rate *= 0.95
+            elif element == 3:
+                self.__attack_variation_rate *= 0.95
+            elif element == 4:
+                self.__breed_will = (self.__breed_will * 2 * 0.95) / 2
+            elif element == 5:
+                self.__variation_rate = (self.__variation_rate * 10 * 0.95) / 10
+            # 确保各项值都在范围内
+            if self.__hp_variation_rate > 1:
+                self.__hp_variation_rate = 1
+            elif self.__hp_variation_rate < 0.0001:
+                self.__hp_variation_rate = 0.0001
+
+            if self.__energy_variation_rate > 1:
+                self.__energy_variation_rate = 1
+            elif self.__energy_variation_rate < 0.0001:
+                self.__energy_variation_rate = 0.0001
+
+            if self.__defence_variation_rate > 1:
+                self.__defence_variation_rate = 1
+            elif self.__defence_variation_rate < 0.0001:
+                self.__defence_variation_rate = 0.0001
+
+            if self.__attack_variation_rate > 1:
+                self.__attack_variation_rate = 1
+            elif self.__attack_variation_rate < 0.0001:
+                self.__attack_variation_rate = 0.0001
+
+            if self.__breed_will > 0.5:
+                self.__breed_will = 0.5
+            elif self.__breed_will < 0.01:
+                self.__breed_will = 0.01
+
+            if self.__variation_rate > 0.35:
+                self.__variation_rate = 0.35
+            elif self.__variation_rate < 0.00001:
+                self.__variation_rate = 0.00001
+            # 亲本体力减为0
+            father.__energy = 0
+            mother.__energy = 0
+
+    @property
+    def bad(self):
+        return self.__bad
+
+    @bad.setter
+    def bad(self, val):
+        """
+        设置细胞的好坏
+        :type val: bool
+        """
+        assert self.bad != val
+        if val:
+            self.bad = True
+            self.__hp_max *= 2
+            self.__hp *= 2
+            self.__energy_max *= 2
+            self.__energy *= 2
+            self.__attack *= 3
+            self.__defence //= 3
+            self.__variation_rate *= 0.1
+        else:
+            self.bad = False
+            if random.randint(0, 100) != 0:
+                self.__hp_max //= 2
+                self.__hp //= 2
+            if random.randint(0, 100) != 0:
+                self.__energy_max //= 2
+                self.__energy //= 2
+            if random.randint(0, 200) != 0:
+                self.__attack //= 3
+            if random.randint(0, 300) != 0:
+                self.__defence *= 3
+            self.__variation_rate *= 9
+            if self.__variation_rate > 0.35:
+                self.__variation_rate = 0.35
+
+    def hurt(self, harm):
+        """
+        对细胞进行攻击
+        :param harm: 对细胞造成的伤害
+        """
+        harm -= self.__defence
+        if harm <= 0:
+            return
+        else:
+            self.__hp -= harm
+            self.__harm_of_round = harm
+            self.__variation_rate += harm / 1000
+            if self.__variation_rate > 0.35:
+                self.__variation_rate = 0.35
+
+    @property
+    def nearly_dead(self):
+        """
+        :return: 细胞是否处于濒死状态
+        """
+        return self.__hp <= 0
+
+    @property
+    def really_dead(self):
+        """
+        :return: 细胞是否处于死亡状态
+        """
+        return self.__hp < -self.hp_max
+
+    @property
+    def hp_percent(self):
+        """
+        :return: 细胞血量的百分比
+        """
+        return self.__hp / self.hp_max
+
+    @property
+    def hp(self):
+        return self.__hp
+
+    def minus_hp(self, val):
+        """
+        给细胞减血，绕过变异率的改变，但没有绕过回合伤害累计
+        :param val: 减血的量
+        """
+        self.__hp -= val
+        self.__harm_of_round += val
+
+    def add_hp(self, val):
+        self.__hp += val
+        self.__variation_rate -= val / 1000
+        if self.__variation_rate <= 0.001:
+            self.__variation_rate = 0.001
+        if self.__hp > self.hp_max:
+            self.__hp = self.hp_max
+
+    @property
+    def usable_energy(self):
+        return self.__energy if not self.nearly_dead else 0
+
+    def minus_energy(self, val):
+        """
+        减少细胞的能量
+        :param val: 减能量减少量
+        """
+        assert self.__energy >= val
+        self.__energy -= val
+
+    def add_energy(self, val):
+        self.__energy += val
+        if self.__energy > self.__energy_max:
+            self.__energy = self.__energy_max
+
+    @property
+    def attack(self):
+        return self.attack
+
+    @property
+    def __need_variation(self):
+        return random.random() <= self.__variation_rate
+
+    @property
+    def __need_upgrade(self):
+        if self.__level >= 100:
+            return False
+        return True if self.__experience >= 20 - 3.5 * (self.__level + 18) + 0.132 * (self.__level + 18) * (
+            self.__level + 18) else False
+
+    @property
+    def level(self):
+        return self.__level
+
+    def upgrade(self):
+        assert self.__need_upgrade
+        self.__level += 1
+        if random.random() < self.__hp_variation_rate:
+            self.__hp += 1
+        if random.random() < self.__energy_variation_rate:
+            self.__energy += 1
+        if random.random() < self.__attack_variation_rate:
+            self.__attack += 1
+        if random.random() < self.__defence_variation_rate:
+            self.__defence += 1
+
+    def add_experience(self, val):
+        """
+        :param val: 增加经验的量
+        """
+        self.__experience += val
+        if self.__experience > Cell.max_experience:
+            self.__experience = Cell.max_experience
+
+    @property
+    def hp_max(self):
+        return self.__hp_max
+
+    @property
+    def energy_max(self):
+        return self.__energy_max
+
+    @property
+    def attack(self):
+        return self.__attack if self.usable_energy > self.__attack else self.usable_energy
+
+    @property
+    def defence(self):
+        return self.__defence
+
+    def round(self):
+        # 增加能量，能量已满则加血
+        if self.__energy == self.__energy_max:
+            if self.__hp == self.__hp_max and random.randint(0, 1) == 0:
+                self.__hp += 1
+        else:
+            self.__energy += 1
+        # 变异
+        if self.__need_variation:
+            self.bad = not self.bad
+        # 升级
+        if self.__need_upgrade:
+            self.upgrade()
+        # 清当前回合损伤计数器
+        self.__harm_of_round = 0
+
+    @property
+    def movable_distance(self):
+        """
+        细胞该回合可移动的距离
+        :rtype : int
+        """
+        if self.nearly_dead:
+            return 0
+        else:
+            return self.usable_energy if self.usable_energy < self.__harm_of_round else self.__harm_of_round
+
+    @property
+    def can_breed(self):
+        return random.randint(0, int(
+            1 / self.__breed_will)) == 0 and self.__harm_of_round == 0 and self.usable_energy == self.__energy_max
---
+++ ground.py
@@ -0,0 +1,223 @@
+import cell
+import random
+import tkinter
+from threading import Timer
+from math import sqrt, degrees, asin
+
+
+class Ground:
+    """
+    场地的Model部分
+    """
+    MAX_COL = 15  # y
+    MAX_ROW = 15  # x
+
+    def __init__(self):
+        self.__cells = []
+        for i in range(0, Ground.MAX_ROW):
+            self.__cells.append([])
+            for j in range(0, Ground.MAX_COL):
+                self.__cells[i].append(None)
+
+    def add_cell(self, x, y, new_cell=cell.Cell()):
+        assert 0 <= x < Ground.MAX_ROW and 0 <= y < Ground.MAX_COL
+        assert self.__cells[x][y] is None
+        self.__cells[x][y] = new_cell
+
+    def __count_cells_around(self, x, y, count_what="all"):
+        ret = 0
+        for xx in range(x - 1 if x - 1 >= 0 else 0, x + 1 if x + 1 < Ground.MAX_ROW else Ground.MAX_ROW - 1):
+            for yy in range(y - 1 if y - 1 >= 0 else 0, y + 1 if y + 1 < Ground.MAX_COL else Ground.MAX_COL - 1):
+                if count_what == "all":
+                    if self.__cells[xx][yy]:
+                        ret += 1
+                elif count_what == "enemy":
+                    if self.__cells[xx][yy] and self.__cells[xx][yy].bad != self.__cells[x][y].bad:
+                        ret += 1
+                elif count_what == "friend":
+                    if self.__cells[xx][yy] and self.__cells[xx][yy].bad == self.__cells[x][y].bad:
+                        ret += 1
+                else:
+                    raise ValueError
+        return ret if count_what == "enemy" else ret - 1  # 数全部和我方时需要除去自己
+
+    def __empty_grid_near(self, x, y):
+        ret = []
+        for xx in range(x - 1 if x - 1 >= 0 else 0, x + 1 if x + 1 < Ground.MAX_ROW else Ground.MAX_ROW - 1):
+            for yy in range(y - 1 if y - 1 >= 0 else 0, y + 1 if y + 1 < Ground.MAX_COL else Ground.MAX_COL - 1):
+                if self.__cells[xx][yy] is None:
+                    ret.append((xx, yy))
+        return ret
+
+    def __do_life_game_rule(self, x, y):
+        if self.__count_cells_around(x, y, "friend") <= 3:
+            self.__cells[x][y].minus_hp(3 - self.__count_cells_around(x, y, "friend"))
+        elif self.__count_cells_around(x, y, "friend") >= 5:
+            self.__cells[x][y].minus_hp(self.__count_cells_around(x, y, "friend") - 5)
+        else:
+            self.__cells[x][y].add_hp(1)
+
+    def __find_random_nearby_enemy_cell(self, x, y):
+        enemies = []
+        for xx in range(x - 1 if x - 1 >= 0 else 0, x + 1 if x + 1 < Ground.MAX_ROW else Ground.MAX_ROW - 1):
+            for yy in range(y - 1 if y - 1 >= 0 else 0, y + 1 if y + 1 < Ground.MAX_COL else Ground.MAX_COL - 1):
+                if self.__cells[xx][yy] and self.__cells[xx][yy].bad != self.__cells[x][y].bad:
+                    enemies.append((xx, yy))
+        return random.sample(enemies, 1)[0] if enemies else None
+
+    def __fight(self, x, y):
+        if not self.__find_random_nearby_enemy_cell(x, y):
+            return
+        xx, yy = self.__find_random_nearby_enemy_cell(x, y)
+        self.__cells[xx][yy].harm(self.__cells[x][y].attack)
+        self.__cells[x][y].miuns_energy(self.__cells[x][y].attack)
+
+    def __find_random_nearby_cell_to_breed(self, x, y):
+        to_breed = []
+        for xx in range(x - 1 if x - 1 >= 0 else 0, x + 1 if x + 1 < Ground.MAX_ROW else Ground.MAX_ROW - 1):
+            for yy in range(y - 1 if y - 1 >= 0 else 0, y + 1 if y + 1 < Ground.MAX_COL else Ground.MAX_COL - 1):
+                if self.__cells[xx][yy] and self.__cells[xx][yy].bad == self.__cells[x][y].bad and \
+                        self.__cells[xx][yy].can_breed:
+                    to_breed.append((xx, yy))
+        return random.sample(to_breed, 1)[0] if to_breed else None
+
+    def __find_random_nearby_empty_grid(self, x, y, passed):
+        grid = []
+        for xx in range(x - 1 if x - 1 >= 0 else 0, x + 1 if x + 1 < Ground.MAX_ROW else Ground.MAX_ROW - 1):
+            for yy in range(y - 1 if y - 1 >= 0 else 0, y + 1 if y + 1 < Ground.MAX_COL else Ground.MAX_COL - 1):
+                if self.__cells[xx][yy] is None and (xx, yy) not in passed:
+                    grid.append((xx, yy))
+        return random.sample(grid, 1)[0] if grid else None
+
+    def __try_breed(self, x, y):
+        if not self.__find_random_nearby_cell_to_breed(x, y) or not self.__find_random_nearby_empty_grid():
+            return
+        bx, by = self.__find_random_nearby_cell_to_breed(x, y)
+        tx, ty = self.__find_random_nearby_empty_grid(x, y)
+        assert self.__cells[bx][by].bad == self.__cells[x][y].bad and self.__cells[tx][ty] is None
+        self.__cells[tx][ty] = cell.Cell(self.__cells[x][y], self.__cells[bx][by])
+
+    def __move(self, x, y):
+        passed = {}
+        d = self.__cells[x][y].movable_distance()
+        for step in range(0, d):
+            if self.__find_random_nearby_empty_grid(x, y, passed):
+                xx, yy = self.__find_random_nearby_empty_grid(x, y, passed)
+                self.__cells[x][y], self.__cells[xx][yy] = None, self.__cells[x][y]
+                passed.add((xx, yy))
+                x, y = xx, yy
+                self.__cells[x][y].minus_energy(1)
+
+    def round(self):
+        # todo tkinter 没有定时器事件，需要用线程模拟！(T_T)（这是controller的工作）
+        for x, row in enumerate(self.__cells):
+            for y, the_cell in enumerate(row):
+                the_cell.round()
+                harm = 0
+                harm += self.__fight(x, y)
+                harm += self.__do_life_game_rule(x, y)
+                self.__move(x, y)
+                self.__try_breed(x, y)
+
+    def get_cell_info_at(self, x, y):
+        return (not self.__cells[x][y].bad), self.__cells[x][y].hp_percent
+
+
+class GroundViewDelegate:
+    def get_size(self):
+        pass
+
+    def get_cell_info_at(self, x, y):
+        pass
+
+    def model_coord(self, x, y):
+        pass
+
+    def view_coord(self, x, y):
+        pass
+
+
+class GroundView:
+    CELL_RADIUS = 20
+
+    def __init__(self, delegate, master_):
+        self.delegate = delegate
+        assert isinstance(self.delegate, GroundViewDelegate)
+        size = self.delegate.get_size()
+        self.canvas = tkinter.Canvas(self, master_, width=(GroundView.CELL_RADIUS * 1.2 * 2 + 1) * 15 + 11,
+                                     height=(GroundView.CELL_RADIUS * 1.2 * 2 + 1) * 15 + 11, bg='white')
+        for x in range(size[0] + 1):
+            self.canvas.create_line(5, x * GroundView.CELL_RADIUS * 1.2 * 2 + 5,
+                                    15 * GroundView.CELL_RADIUS * 1.2 * 2 + 5, x * GroundView.CELL_RADIUS * 1.2 * 2 + 5)
+        for y in range(size[1] + 1):
+            self.canvas.create_line(y * GroundView.CELL_RADIUS * 1.2 * 2 + 5, 5,
+                                    y * GroundView.CELL_RADIUS * 1.2 * 2 + 5, 15 * GroundView.CELL_RADIUS * 1.2 * 2 + 5)
+
+    def draw_cell(self, x, y, is_good, percent):
+        arc_coord = x - GroundView.CELL_RADIUS, y - GroundView.CELL_RADIUS, x + GroundView.CELL_RADIUS, y + GroundView.CELL_RADIUS
+        delta_h = 2 * GroundView.CELL_RADIUS * abs(percent) - GroundView.CELL_RADIUS
+        delta_x = sqrt(GroundView.CELL_RADIUS * GroundView.CELL_RADIUS - delta_h * delta_h)
+        if percent >= 0.1:
+            if percent >= 0.99:
+                self.canvas.create_oval(arc_coord, fill='green', outline='black' if is_good else 'red')
+            else:
+                r = (int(510 - 510 * percent) if percent >= 0.5 else 255) * 16 * 16 * 16 * 16
+                g = int(255 if percent >= 0.5 else percent * 2 * 255) * 16 * 16
+                c = '#%06x' % (r + g)
+                tri_coord = x - delta_x, y - delta_h, \
+                            x + delta_x, y - delta_h, \
+                            x, y
+                self.canvas.create_arc(arc_coord, start=degrees(asin(2 * percent - 1)),
+                                       extent=-180 - 2 * degrees(asin(2 * percent - 1)), fill=c,
+                                       outline=c)
+                self.canvas.create_polygon(tri_coord, fill=c if percent >= 0.5 else 'white',
+                                           outline=c if percent >= 0.5 else 'white', width=1 if percent > 0.5 else 2)
+        elif percent <= -0.1:
+            if percent <= - 0.99:
+                self.canvas.create_oval(arc_coord, fill='grey', outline='black' if is_good else 'red')
+            else:
+                tri_coord = x - delta_x, y + delta_h, \
+                            x + delta_x, y + delta_h, \
+                            x, y
+            self.canvas.create_arc(arc_coord, start=-degrees(asin(2 * -percent - 1)),
+                                   extent=180 + 2 * degrees(asin(2 * -percent - 1)), fill='grey', outline='grey')
+            self.canvas.create_polygon(tri_coord, fill='grey' if -percent >= 0.5 else 'white',
+                                       outline='grey' if -percent >= 0.5 else 'white',
+                                       width=1 if abs(percent) > 0.5 else 3)
+        self.canvas.create_oval(arc_coord, outline='black' if is_good else 'red')
+
+    def clear(self, x, y):
+        arc_coord = x - GroundView.CELL_RADIUS, y - GroundView.CELL_RADIUS, x + GroundView.CELL_RADIUS, y + GroundView.CELL_RADIUS
+        self.canvas.create_oval(arc_coord, fill='white', outline='white')
+
+    def redraw(self):
+        size = self.delegate.get_size()
+        for x in range(size[0]):
+            for y in range(size[1]):
+                if self.delegate.get_cell_info_at(x, y) is None:
+                    self.clear(self.delegate.view_coord(x, y))
+                else:
+                    self.draw_cell(self.delegate.view_coord(x, y), self.delegate.get_cell_info_at(x, y))
+
+
+class GroundViewController(GroundViewDelegate):
+    def on_timer(self):
+        self.model.round()
+        self.view.redraw()
+
+    def __init__(self, master_view):
+        self.model = Ground()
+        self.view = GroundView(self, master_view)
+        self.timer = Timer(5, self.on_timer)
+
+    def get_size(self):
+        return self.model.MAX_ROW, self.model.MAX_COL
+
+    def get_cell_info_at(self, x, y):
+        return self.model.get_cell_info_at(x, y)
+
+    def model_coord(self, x, y):
+        return int((x - 5) // (GroundView.CELL_RADIUS * 1.2 * 2)), int((y - 5) // (GroundView.CELL_RADIUS * 1.2 * 2))
+
+    def view_coord(self, x, y):
+        return x * GroundView.CELL_RADIUS * 1.2 * 2 + GroundView.CELL_RADIUS * 1.2 + 5, y * GroundView.CELL_RADIUS * 1.2 * 2 + GroundView.CELL_RADIUS * 1.2 + 5
