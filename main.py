bdaecef9a0a576562c3cf3dd2dec0685006812b4
longfangsong <longfangsong@icloud.com>
2015-08-14 17:18:29 +0800

    Add user control (View only)

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import tkinter
 from ground import GroundViewController
+from user_control import *
 
 
-class SuperCellViewDelegate:
-    @property
-    def sub_view(self):
-        return None
-
-
-class SuperCellViewController:
+class SuperCellViewController(UserControlDelegate):
     def __init__(self):
         self.view = tkinter.Tk()
         self.ground = GroundViewController(self.view)
         self.ground.view.grid(0, 0)
+        self.user_control_interface = UserControlView(self, self.view)
+        self.user_control_interface.pack(1, 0)
         self.view.mainloop()
 
 
 c = SuperCellViewController()
-
---
+++ user_control.py
@@ -0,0 +1,38 @@
+import tkinter
+
+
+class UserControlDelegate:
+    def speed_changed(self, speed_val):
+        pass
+
+
+class UserControlView():
+    def __init__(self, delegate_, master_=None):
+        assert isinstance(delegate_, UserControlDelegate)
+        self.view = tkinter.Frame(master_)
+        self.delegate = delegate_
+        self.toggle_speed = tkinter.Scale(self.view, orient=tkinter.HORIZONTAL, from_=0, to=100, resolution=1,
+                                          label="卷动速度")
+        self.toggle_speed.grid(column=0, columnspan=3, row=0)
+        slower_button = tkinter.Button(self.view, text="slower", command=self.slower)
+        slower_button.grid(column=0, row=1)
+        pause_button = tkinter.Button(self.view, text="pause", command=self.pause)
+        pause_button.grid(column=1, row=1)
+        faster_button = tkinter.Button(self.view, text="faster", command=self.faster)
+        faster_button.grid(column=2, row=1)
+
+
+    def pause(self):
+        self.toggle_speed.set(0)
+        self.delegate.speed_changed(0)
+
+    def slower(self):
+        self.toggle_speed.set(self.toggle_speed.get() - 5)
+        self.delegate.speed_changed(self.toggle_speed.get())
+
+    def faster(self):
+        self.toggle_speed.set(self.toggle_speed.get() + 5)
+        self.delegate.speed_changed(self.toggle_speed.get())
+
+    def pack(self, col, row_):
+        self.view.grid(column=col, row=row_)
